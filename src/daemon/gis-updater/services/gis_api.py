310b15e72a2b7ce12e9f3c9c3661b2bee5d343bb
Diogo Assunção <dassuncao1234@gmail.com>
2024-01-03 21:27:29 +0000

    refactor: Improve and fix updater-gis

--- src/daemon/gis-updater/services/gis_api.py
+++ src/daemon/gis-updater/services/gis_api.py
@@ -7,7 +7,7 @@ from services.api import Api
 class GisApi(Api):
     def __init__(self) -> None:
         super().__init__(Env.get_var("API_GIS_URL"))
-        self.waiting_time = Env.get_var("GIS_WAIT_FOR_API")
+        self.waiting_time = int(Env.get_var("GIS_WAIT_FOR_API"))
 
     async def publish_coordinates(self, region: str, lat: float, lon: float):
         try:
@@ -16,11 +16,7 @@ class GisApi(Api):
                 "lon": lon
             }
 
-            await self._make_request("POST", f"/markers/{region}", json=request_body)
-            time.sleep(1)
-        except httpx.HTTPError as http_err:
-            print(f"HTTP error occurred: {http_err}")
-            raise
+            await self._make_request("PATCH", f"api/entity/{region}", json=request_body)
+            time.sleep(self.waiting_time)
         except Exception as e:
-            print(f"An unexpected error occurred: {e}")
-            raise
+            print(f"Cound't Patch entity: {e}")
