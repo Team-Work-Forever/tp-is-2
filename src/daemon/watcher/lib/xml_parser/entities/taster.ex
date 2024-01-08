d3b911e1718dd7ae7fbc20bc543062f79fcfed8e
Diogo Assunção <dassuncao1234@gmail.com>
2024-01-08 13:48:31 +0000

    modified: Modify Watcher to only check new notifications and create task to 'create-entity' to entity queue

--- src/daemon/watcher/lib/xml_parser/entities/taster.ex
+++
@@ -1,9 +0,0 @@
-defmodule XmlParser.Entities.Taster do
-  @type t :: %__MODULE__{
-          id: String.t(),
-          name: String.t(),
-          twitter_handle: String.t()
-        }
-
-  defstruct [:id, :name, :twitter_handle]
-end
