85c018c3d3af9d5529e9e4a86828fee837063ee7
Diogo Assunção <dassuncao1234@gmail.com>
2024-01-11 13:17:17 +0000

    modified: Modify Api-Entities fix the validation

--- src/api/entities/run.sh
+++ src/api/entities/run.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
-
 export DATABASE_URL="postgresql://${PG_REL_USER}:${PG_REL_PASSWORD}@${PG_REL_HOST}:${PG_REL_PORT}/${PG_REL_DATABASE}"
 
-# RUN npm install;
-# RUN npx prisma generate --schema=./src/config/prisma/schema.prisma;
-
-# if [ "$USE_DEV_MODE" = "true" ];
-#   then npm run start:dev;
-#   else npm run start:prod;
-# fi
-
-npm run start:prod;
+if [ "$USE_DEV_MODE" = "true" ];
+  then npm run start:dev;
+  else npm run start:prod;
+fi
--- src/api/entities/src/main.ts
+++ src/api/entities/src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(express.json());
+  app.enableCors();
 
   await app.listen(process.env.API_PORT);
 }
