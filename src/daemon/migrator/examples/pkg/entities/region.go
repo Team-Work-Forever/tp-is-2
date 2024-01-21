6d0876a35fcc0cdaf05d412c2c1d99295a9858b8
Diogo Assunção <dassuncao1234@gmail.com>
2024-01-21 22:06:03 +0000

    clean: Fix Project Structure and Setup Ready to build with docker

--- src/daemon/migrator/examples/pkg/entities/region.go
+++
@@ -1,9 +0,0 @@
-package entities
-
-type Region struct {
-	Id        string
-	Region    string  `json:"name"`
-	Province  string  `json:"province"`
-	Latitude  float32 `json:"lat"`
-	Longitude float32 `json:"lon"`
-}
--- src/daemon/migrator/examples/pkg/entities/taster.go
+++
@@ -1,7 +0,0 @@
-package entities
-
-type Taster struct {
-	Id            string
-	Name          string `json:"name"`
-	TwitterHandle string `json:"twitterHandle"`
-}
